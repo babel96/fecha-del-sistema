f19e73972ff215b9f2bbf91ec71aa193acc7be8a
babel96 <49528963+babel96@users.noreply.github.com>
2019-09-20 09:43:17 -0300

    logre entender la fecha lpm

--- time.cpp
+++ time.cpp
@@ -1,11 +1,23 @@
 #include <cstdio>
 #include <ctime>
+#include <iostream>
 
+
+using namespace std;
+
+struct Fecha{
+
+    int dia;
+    int mes;
+    int anio;
+
+};
 int main()
 {
   time_t tiempo;
   char cad[80];
   struct tm *tmPtr;
+  struct Fecha x;
 
   tiempo = time(NULL);
   tmPtr = localtime(&tiempo);
@@ -13,6 +25,12 @@ int main()
   printf("FECHA ACTUAL \n%d/%d/%d %d:%d:%d", tmPtr->tm_mday, tmPtr->tm_mon+1, 1900+tmPtr->tm_year, tmPtr->tm_hour, tmPtr->tm_min, tmPtr->tm_sec);
   strftime(cad, 80, "%A %d de %B de %Y", tmPtr);
   printf("\n\n%s", cad);
+
+  x.dia=tmPtr->tm_mday;
+ cout <<"\n\nESTE ES EL DIA: ";
+ cout <<x.dia;
+
+
 
   return 0;
 }
